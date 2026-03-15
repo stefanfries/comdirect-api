66de81b5def0c41beaed9a2184e056b9a632b416
Stefan Fries <stefan.fries.burgdorf@gmx.de>
2026-03-15 23:20:21 +0100

    fix(settings): add extra='ignore' to ClientSettings
    Prevents ValidationError when .env contains MONGODB_* fields
    (used by SyncSettings) that ClientSettings does not declare.

--- src/comdirect_api/settings.py
+++ src/comdirect_api/settings.py
@@ -12,7 +12,7 @@ class ClientSettings(BaseSettings):
     zugangsnummer: SecretStr
     pin: SecretStr
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
 
 settings = ClientSettings()
